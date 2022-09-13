f2abf361e683562b05581cbbd666497591f47fbc
Your Name <praful.dudhewar2@gmail.com>
2022-09-13 23:38:18 +0530

    Added code for LinkedHashSet

--- MainProject/src/com/praful/collections/HashSet.java
+++ MainProject/src/com/praful/collections/HashSet.java
@@ -1,9 +1,13 @@
 package com.praful.collections;
 
-public class LinkedHashSet {
+public class HashSet {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+
+		System.out.println("Hello");
+		
+		
 
 	}
 
